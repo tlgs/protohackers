2fb7b08569bf5ee27ce9f440f4c509affca0c243
Tiago Seabra <tlgs@users.noreply.github.com>
2022-10-21 17:32:10 +0100

    Use bufio.Reader's `ReadString` instead of a Scanner
    some other minor renaming

--- cmd/mob-in-the-middle/main.go
+++ cmd/mob-in-the-middle/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"context"
 	"io"
 	"log"
@@ -16,15 +15,6 @@ import (
 
 var boguscoin = regexp.MustCompile(`^7[a-zA-Z0-9]{25,34}$`)
 
-// This is a custom implementation of bufio.ScanLines so that the last
-// non-empty line of input is not returned. Also, not stripping `\r`.
-func customScanLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if i := bytes.IndexByte(data, '\n'); i >= 0 {
-		return i + 1, data[0:i], nil
-	}
-	return 0, nil, nil
-}
-
 type MobInTheMiddle struct{ *protohackers.Config }
 
 func (MobInTheMiddle) Setup() context.Context { return context.TODO() }
@@ -35,41 +25,39 @@ func (MobInTheMiddle) Handle(_ context.Context, downstream net.Conn) {
 
 	upstream, err := net.Dial("tcp", "chat.protohackers.com:16963")
 	if err != nil {
-		log.Print(err)
+		log.Printf("%s (%v)", err, addr)
 		downstream.Close()
 		return
 	}
 
-	// The following _patching proxy_ implementation follows a couple of ideas discussed in:
+	// The following relay implementation follows a couple of ideas discussed in:
 	// <https://stackoverflow.com/q/25090690/5818220>
 	// <https://gist.github.com/jbardin/821d08cb64c01c84b81a>
 	once := sync.Once{}
-	patchedCopy := func(src io.ReadCloser, dst io.WriteCloser) {
-		defer once.Do(
-			func() {
-				src.Close()
-				dst.Close()
-				log.Printf("closed connection: %v", addr)
-			},
-		)
+	relay := func(dst io.WriteCloser, src io.ReadCloser) {
+		defer once.Do(func() { src.Close(); dst.Close(); log.Printf("closed connection: %v", addr) })
 
-		scanner := bufio.NewScanner(src)
-		scanner.Split(customScanLines)
-
-		for scanner.Scan() {
-			parts := make([]string, 0)
-			for _, part := range strings.Split(scanner.Text(), " ") {
-				s := boguscoin.ReplaceAllString(part, "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
-				parts = append(parts, s)
+		for r := bufio.NewReader(src); ; {
+			msg, err := r.ReadString('\n')
+			if err != nil {
+				return
 			}
 
-			out := strings.Join(parts, " ") + "\n"
-			dst.Write([]byte(out))
+			tokens := make([]string, 0, 8)
+			for _, raw := range strings.Split(msg[:len(msg)-1], " ") {
+				t := boguscoin.ReplaceAllString(raw, "7YWHMfk9JZe0LM0g1ZauHuiSxhI")
+				tokens = append(tokens, t)
+			}
+
+			out := strings.Join(tokens, " ") + "\n"
+			if _, err = dst.Write([]byte(out)); err != nil {
+				log.Printf("%s (%v)", err, addr)
+			}
 		}
 	}
 
-	go patchedCopy(upstream, downstream)
-	patchedCopy(downstream, upstream)
+	go relay(downstream, upstream)
+	relay(upstream, downstream)
 }
 
 func main() {
